ea7cc3689d776646a176688a3ecba22adeab89d3
Bertrand Benoit <hemerais@bertrand-benoit.net>
2010-09-13 14:57:17 +0000

    Added new configuration elements for environment (java home and ant home), needed for source code management.

--- hemera/scripts/utilities.sh
+++ hemera/scripts/utilities.sh
@@ -94,3 +94,48 @@ function getConfigPath() {
   echo "$installDir/$value"  
 }
 
+#########################
+## Functions - source code management
+# usage: manageJavaHome
+function manageJavaHome() {
+  # Checks if environment variable JAVA_HOME is defined.
+  if [ -z "$JAVA_HOME" ]; then
+    # Checks if it is defined in configuration file.
+    javaHome=$( getConfigValue "$CONFIG_KEY.java.home" ) || exit 100
+    [ -z "$javaHome" ] && errorMessage "You must either configure JAVA_HOME environment variable or $CONFIG_KEY.java.home configuration element."
+    
+    # Ensures it exists.
+    [ ! -d "$javaHome" ] && errorMessage "$CONFIG_KEY.java.home defined $javaHome which is not found."
+    
+    export JAVA_HOME="$javaHome"
+  fi
+  
+  # Ensures it is a jdk home directory.
+  local _javaPath="$JAVA_HOME/bin/java"
+  local _javacPath="$JAVA_HOME/bin/javac"
+  [ ! -f "$_javaPath" ] && errorMessage "Unable to find java binary, ensure '$JAVA_HOME' is the home of a Java Development Kit version 6."
+  [ ! -f "$_javacPath" ] && errorMessage "Unable to find javac binary, ensure '$JAVA_HOME' is the home of a Java Development Kit version 6."
+  
+  writeMessage "Found: $( "$_javaPath" -version 2>&1|head -n 1 )"
+}
+
+# usage: manageAntHome
+function manageAntHome() {
+  # Checks if environment variable ANT_HOME is defined.
+  if [ -z "$ANT_HOME" ]; then
+    # Checks if it is defined in configuration file.
+    antHome=$( getConfigValue "$CONFIG_KEY.ant.home" ) || exit 100
+    [ -z "$antHome" ] && errorMessage "You must either configure ANT_HOME environment variable or $CONFIG_KEY.ant.home configuration element."
+    
+    # Ensures it exists.
+    [ ! -d "$antHome" ] && errorMessage "$CONFIG_KEY.ant.home defined $antHome which is not found."
+    
+    export ANT_HOME="$antHome"
+  fi
+  
+  # Checks ant is available.
+  local _antPath="$ANT_HOME/bin/ant"
+  [ ! -f "$_antPath" ] && errorMessage "Unable to find ant binary, ensure '$ANT_HOME' is the home of a Java Development Kit version 6."
+  
+  writeMessage "Found: $( "$_antPath" -v 2>&1|head -n 1 )"
+}

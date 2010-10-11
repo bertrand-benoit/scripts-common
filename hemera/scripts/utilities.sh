b68f0084ca73b2a962758aff47dc208ebe95f3a4
Bertrand Benoit <hemerais@bertrand-benoit.net>
2010-10-11 07:15:12 +0000

    Added license information all scripts and ant build file. Updated help.sh consequently.

--- hemera/scripts/utilities.sh
+++ hemera/scripts/utilities.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 #
-# Author: Bertrand BENOIT <projettwk@users.sourceforge.net>
+# Hemera - Intelligent System (https://sourceforge.net/projects/hemerais)
+# Copyright (C) 2010 Bertrand Benoit <projettwk@users.sourceforge.net>
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, see http://www.gnu.org/licenses
+# or write to the Free Software Foundation,Inc., 51 Franklin Street,
+# Fifth Floor, Boston, MA 02110-1301  USA
+#
 # Version: 1.0
 # Description: provides lots of utilities functions.
 
